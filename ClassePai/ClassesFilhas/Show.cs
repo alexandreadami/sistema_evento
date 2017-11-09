5d052879068367d070613a3c970837e83f50f9e7
ALEXANDRE ADAMI <55349862120@jpmorgan.sp>
2017-11-08 22:02:29 -0200

    inicio

---
+++ ClassePai/ClassesFilhas/Show.cs
@@ -0,0 +1,28 @@
+using System;
+namespace ProjetoEvento.ClassePai.ClassesFilhas
+{
+    public class Show:Evento
+    {
+        public string Artista { get; set; }
+        public string GeneroMusical { get; set; }
+        
+        public Show(){
+
+        }
+
+        public Show(string Titulo, string Local, int Lotacao, string Duracao, int Classificacao, DateTime Data, string Artista, string GeneroMusical){
+            base.Titulo = Titulo;
+            base.Local = Local;
+            base.Duracao = Duracao;
+            base.Classificacao = Classificacao;
+            base.Data = Data;
+            this.Artista = Artista;
+            this.GeneroMusical = GeneroMusical;
+        }
+
+        public override bool Cadastrar(){
+            return false;
+        }
+    
+    }
+}
